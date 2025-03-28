40aaf736f7a3dd92ccd3d7224a21af26bdad1bf5
fromjkeee16 <fromjkeee16@gmail.com>
2025-03-28 22:18:20 +0200

    remove (temporary) unused type

--- src/types/SerializeableTypes.ts
+++
@@ -1,9 +0,0 @@
-export type SerializableValue =
-  | string
-  | number
-  | boolean
-  | null
-  | SerializableObject
-  | SerializableArray;
-export type SerializableObject = { [key: string]: SerializableValue };
-export type SerializableArray = SerializableValue[];
